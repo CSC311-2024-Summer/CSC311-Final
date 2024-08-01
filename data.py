7604634e02c294ef9f236aa5c2cec963a3d7a8a6
MarkZhang3 <72476129+MarkZhang3@users.noreply.github.com>
2024-08-01 12:14:55 -0400

    updated parse results and added parse pp data

--- data.py
+++ data.py
@@ -1,11 +1,27 @@
 import xml.etree.ElementTree as ET
 import csv
 import os
-from models import Racer, Race
-from datetime import datetime
+from models import Racer, Race, Horse, PastPerformance
+from datetime import datetime, timedelta
 
 
-def update_racer(element, cur_racer, prefix=''):
+def parse_element_helper(element, parent_path=""):
+    results = []
+    for child in element:
+        child_path = f"{parent_path}/{child.tag}" if parent_path else child.tag
+        if child.text and child.text.strip():
+            results.append(f"{child_path}: {child.text.strip()}")
+        results.extend(parse_element_helper(child, child_path))
+    return results
+
+
+def get_nested_data_as_string(element):
+    parsed_data = parse_element_helper(element)
+    return ", ".join(parsed_data)
+
+
+# result charts data:
+def update_racer_result_charts(element, cur_racer, prefix=''):
     for child in element:
         column_name = f"{prefix}{child.tag}" if prefix else child.tag
         if column_name in Racer.xml_headers_mapping():
@@ -13,10 +29,15 @@ def update_racer(element, cur_racer, prefix=''):
             attr, attr_type = Racer.xml_headers_mapping()[column_name]
             converted_val = attr_type(child.text)
             setattr(cur_racer, attr, converted_val)
-        update_racer(child, cur_racer, f"{column_name}/")
+
+            # seperate case (not automated)
+            if column_name == 'ENTRY/LAST_PP':
+                cur_racer.last_pp = get_nested_data_as_string(child)
+
+        update_racer_result_charts(child, cur_racer, f"{column_name}/")
 
 
-def parse_xml(file_path):
+def parse_xml_result_charts(file_path):
     tree = ET.parse(file_path)
     root = tree.getroot()
     file_date = root.get('RACE_DATE')
@@ -30,20 +51,95 @@ def parse_xml(file_path):
 
         for racer in race.findall('ENTRY'):
             cur_racer = Racer()
-            update_racer(racer, cur_racer, 'ENTRY/')
-            if isinstance(cur_racer, str):
-                raise AttributeError
+            update_racer_result_charts(racer, cur_racer, 'ENTRY/')
             cur_race.add_racer(cur_racer)
-        if isinstance(cur_race.racers, str):
-            raise AttributeError
+
         races.append(cur_race)
 
     return races
 
 
-def write_to_csv(races, output_csv):
-    # Collect all headers
+# pp data:
+def update_horse_pp(element, cur_pp, prefix=''):
+    for child in element:
+        column_name = f"{prefix}{child.tag}" if prefix else child.tag
+        if column_name in PastPerformance.xml_headers_mapping():
+            # print(column_name, child.text)
+            attr, attr_type = PastPerformance.xml_headers_mapping()[column_name]
+            try:
+                converted_val = attr_type(child.text)
+                setattr(cur_pp, attr, converted_val)
+            except TypeError as e:
+                print(child.text, child.tag)
+
+        update_horse_pp(child, cur_pp, f"{column_name}/")
+
+
+def parse_xml_pp(file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+
+    horse_info = []
+    for race in root.findall('Race'):
+        for starter in race.findall('Starters'):
+            horse = starter.find('Horse')
+            cur_horse = Horse()
+            for column, (attr, attr_type) in Horse.xml_headers_mapping().items():
+                # if column == 'BreedType/Value':
+                #     cur_horse.breed_type = horse.find('BreedType').find('Value').text.strip()
+                #     continue
+                try:
+                    converted_val = attr_type(horse.find(column).text.strip()) if horse.find(column) is not None else ''
+                    setattr(cur_horse, attr, converted_val)
+                except ValueError or TypeError as e:
+                    print(e, horse.find(column).tag, column)
+
+            for past_performance in starter.findall('PastPerformance'):
+                cur_pp = PastPerformance()
+                update_horse_pp(past_performance, cur_pp, '/')
+
+                # handling Fractions because it has special cases
+                for fractions in root.findall('Fractions'):
+                    fraction = fractions.find('Fraction').text
+                    # cur_pp.finish_time += int(fractions.find('Time').text)
+                    if fraction == 'W':
+                        cur_pp.finish_time = fractions.find('Time').text
+                        break
+
+                cur_pp.update()
+
+                cur_horse.add_pp(cur_pp)
+
+            horse_info.append(cur_horse)
+
+    return horse_info
+
+
+def write_to_csv_pp(horse_files, output_csv):
+    headers = ['reg_num', 'sex', 'age'] + list(PastPerformance().to_csv_format().keys())
+    headers.remove('wind_dir')
+    headers.remove('wind_speed')
+    headers.remove('distance_unit')
+    headers.remove('race_number')
+
+    with open(output_csv, mode='w', newline='', encoding='utf-8') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=headers)
+        writer.writeheader()
+        # for horses in horse_files:
+        #     for horse in horses:
+        #         data = horse.to_csv_format()
+        #         writer.writerows(data)
+        for horse in horse_files:
+            data = horse.to_csv_format()
+            # print('data', data)
+            writer.writerows(data)
+
+
+def write_to_csv_results(races, output_csv):
+    # create headers based on the max number of racers within a race
+    max_racers = max(race.num_of_racers for race in races)
     headers = races[0].to_list()[0]
+    headers += [f'racer_{i}_{key}' for i in range(max_racers) for key in Racer().to_dict().keys()]
 
     # Write to CSV
     with open(output_csv, mode='w', newline='', encoding='utf-8') as csv_file:
@@ -56,21 +152,52 @@ def write_to_csv(races, output_csv):
             writer.writerow(race.to_list()[1])
 
 
-def parse_all_files(directory, output_csv):
+def parse_all_files_results(directory, output_csv):
     races = []
+    # nums = {}
     for file in os.listdir(directory):
-        race_lis = parse_xml(os.path.join(directory, file))
-        for race in race_lis:
-            if isinstance(race.racers, str):
-                raise AttributeError
+        race_lis = parse_xml_result_charts(os.path.join(directory, file))
+        # for race in race_lis:
+        #     if isinstance(race.racers, str):
+        #         raise AttributeError
+        #     if race.num_of_racers not in nums:
+        #         nums[race.num_of_racers] = 1
+        #     else:
+        #         nums[race.num_of_racers] += 1
         races += race_lis
 
-    write_to_csv(races, output_csv)
+    write_to_csv_results(races, output_csv)
+    # print(nums)
+
+
+def parse_all_files_pp(directory, output_csv):
+    horses = []
+    for file in os.listdir(directory):
+        horse_lis = parse_xml_pp(os.path.join(directory, file))
+        horses += horse_lis
+
+    write_to_csv_pp(horses, output_csv)
+
+
+def aggregate_previous_year_averages(df, row, column='official_finish', range=365 * 3):
+    """
+    :param df:
+    :param column: precondtion - df[column] must be summable (float or int)
+    :return:
+    """
+    filtered_df = df[(df['reg_num'] == row['reg_num'])
+                     & (df['race_date'] >= (row['race_date'] - timedelta(days=range)))
+                     & (df['race_date'] <= row['race_date'])]
+
+    if filtered_df.empty:
+        return 0
+
+    return filtered_df[column].mean()
 
 
 if __name__ == '__main__':
     data_dir = './data/2023 Result Charts'
-    output_csv = './data/test.csv'
+    output_csv = './data/output.csv'
 
     # TESTING
     # data_file = './data/2023 Result Charts/wo20230422tch.xml'
@@ -82,4 +209,28 @@ if __name__ == '__main__':
     #
     # print(races[0].racers[0].to_dict())
 
-    parse_all_files(data_dir, output_csv)
+    # parse_all_files_results(data_dir, output_csv)
+
+    data_dir_pp = './data/2023 PPs'
+    output_csv_pp = './data/pp_output_new.csv'
+    # parse_all_files_pp(data_dir_pp, output_csv_pp)
+
+    import pandas as pd
+
+    # df = pd.read_csv(output_csv_pp)
+    # df['race_date'] = pd.to_datetime(df['race_date'], format='%Y-%m-%d+%H:%M')
+    # earliest_date = df['race_date'].min()
+    # print('earliest date is: ', earliest_date)
+    #
+    # threshold_date = pd.to_datetime('2020-01-01')
+    # df_clean = df[df['race_date'] > threshold_date]
+    #
+    # df_clean['scratched'] = df_clean['finish_time'] == 0
+    # df_clean['avg_official_finish'] = df_clean.apply(lambda row: aggregate_previous_year_averages(df_clean, row, 'official_finish'), axis=1)
+    # df_clean = df_clean.drop(columns=['official_finish', 'time_of_horse'])
+    # df_clean.to_csv('./data/cleaned_output_pp_new.csv', index=False)
+
+    # round avg official finish time to nearest 0.5
+    df = pd.read_csv('./data/cleaned_output_pp_new.csv')
+    df['avg_official_finish'] = df['avg_official_finish'].apply(lambda x: round(x * 2) / 2)
+    df.to_csv('./data/pp_output.csv', index=False)
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import sys
+from datetime import datetime
 
 
 class Racer:
@@ -7,23 +8,35 @@ class Racer:
         self.equip = ''
         self.weight = 0.0
         self.post_position = ''
-        self.offical_finish = ''
+        self.official_finish = ''
         self.age = 0
         self.jockey_key = 0
         self.trainer_key = 0
+        # self.breed_type = '' # always TB
+        self.last_pp = ''
+        self.meds = ''
+        self.dollar_odds = 0.0
+        self.comments = ''
 
     @staticmethod
     def xml_headers_mapping():
-        return {'ENTRY/NAME': ('name', str), 'ENTRY/EQUIP': ('equip', str), 'ENTRY/WEIGHT': ('weight', float),
-                'ENTRY/POST_POS': ('post_position', str), 'ENTRY/OFFICAL_FIN': ('official_finish', str),
-                'ENTRY/AGE': ('age', int), 'ENTRY/JOCKEY/KEY': ('jockey_key', int),
-                'ENTRY/TRAINER/KEY': ('trainer_key', int)}
+        return {
+            'ENTRY/NAME': ('name', str), 'ENTRY/EQUIP': ('equip', str), 'ENTRY/WEIGHT': ('weight', float),
+            'ENTRY/POST_POS': ('post_position', str),
+            'ENTRY/OFFICIAL_FIN': ('official_finish', str),
+            'ENTRY/AGE': ('age', int), 'ENTRY/JOCKEY/KEY': ('jockey_key', int),
+            'ENTRY/TRAINER/KEY': ('trainer_key', int), 'ENTRY/LAST_PP': ('last_pp', str),
+            'ENTRY/MEDS': ('meds', str), 'ENTRY/DOLLAR_ODDS': ('dollar_odds', float),
+            # 'ENTRY/BREED': ('breed_type', str), # always TB
+            'ENTRY/COMMENT': ('comments', str)
+        }
 
     def to_dict(self):
-        return self.__dict__
+        return self.__dict__.copy()
 
     def to_csv_format(self):
         # print(','.join(str(value) for key, value in self.__dict__.items()))
+        # return [str(key) + ': ' + str(value) for key, value in self.__dict__.items()]
         return [str(value) for key, value in self.__dict__.items()]
 
 
@@ -64,10 +77,189 @@ class Race:
         for racer in self.racers:
             raw_dict['racers'] += racer.to_csv_format()
 
-        headers = [key for key in raw_dict.keys() if key != 'racers'] + ['racers']
+        headers = [key for key in raw_dict.keys() if key != 'racers']
+        headers.remove('distance_unit')
         values = [str(raw_dict[key]) for key in headers if key != 'racers'] + raw_dict['racers']
+        values[headers.index('distance')] = self.convert_distance_to_meters()
 
         return headers, values
 
-    def update_distance(self):
+    def to_csv_format(self):
+        raw_dict = self.__dict__.copy()
+        del raw_dict['racers']
+
+        for i, racer in enumerate(self.racers, start=1):
+            for key, value in racer.to_dict():
+                raw_dict[f'racer_{i}_{key}'] = value
+
+        return raw_dict
+
+    def convert_distance_to_meters(self):
+        if self.distance_unit == 'F':
+            return round(self.distance / 25) * 201.168 * 25 / 100
+        if self.distance_unit == 'Y':
+            return self.distance * 0.9144
+        if self.distance_unit == 'M':
+            return (self.distance / 2) * 201.168
+
+
+class PastPerformance:
+    def __init__(self):
+        self.num_of_starters = 0
+        self.race_date = ''
+        self.run_up_distance = 0
+        self.race_number = 0
+        self.race_comments = ''
+        self.track_condition = ''
+        self.wind_dir = ''
+        self.wind_speed = 0.0
+        self.temperature = ''
+        self.distance = 0.0
+        self.distance_unit = ''
+        self.weather = ''
+        self.post_position = ''
+        self.official_finish = ''
+        self.time_of_horse = ''
+        self.equipment = ''
+        self.jockey = 0
+        self.weight_carried = 0
+        self.meds = ''
+        self.dollar_odds = 0.0
+        self.finish_time = 0
+        self.owner = ''
+        self.trainer = ''
+
+    @staticmethod
+    def xml_headers_mapping():
+        d_temp = {
+            '/NumberOfStarters': ('num_of_starters', int),
+            '/RaceDate': ('race_date', str),
+            '/RunUpDistance': ('run_up_distance', str),
+            '/RaceNumber': ('race_number', int),
+            '/Start/LongComment': ('race_comments', str),
+            # RaceComment under PastPerformance is always blank
+            '/TrackCondition/Value': ('track_condition', str),
+            '/WindDirection': ('wind_dir', str),
+            '/WindSpeed': ('wind_speed', float),
+            '/Temperature': ('temperature', str),
+            '/Distance/DistanceId': ('distance', float),
+            '/Distance/DistanceUnit/Value': ('distance_unit', str),
+            '/Weather': ('weather', str),
+            '/Start/PostPosition': ('post_position', str),
+            '/Start/OfficialFinish': ('official_finish', float),
+            '/Start/TimeOfHorse': ('time_of_horse', str),
+            '/Start/Equipment/Value': ('equipment', str),
+            '/Start/Jockey/ExternalPartyId': ('jockey', int),
+            '/Start/WeightCarried': ('weight_carried', float),
+            '/Start/Medication/Description': ('meds', str),  # need inner parsing
+            '/Start/Odds': ('dollar_odds', float),
+            '/Fractions/Time': ('finish_time', str),  # only if Fractions/Fraction == 'W'
+            '/Start/Owner/ExternalPartyId': ('owner', str),
+            '/Start/Trainer/ExternalPartyId': ('trainer', str)
+        }
+
+        # new_d = {}
+        #
+        # for key in d_temp:
+        #     new_d['Starter/PastPerformance' + key] = d_temp[key]
+        #
+        # return new_d
+        return d_temp
+
+    def to_list(self):
+        return [str(key) + ': ' + str(value) for key, value in self.__dict__.items()]
+
+    def to_csv_format(self):
+        d = self.__dict__.copy()
+        return d
+
+    def convert_distance(self):
+        if self.distance_unit == 'F':
+            return round(self.distance / 25) * 201.168 * 25 / 100
+        if self.distance_unit == 'Y':
+            return self.distance * 0.9144
+        if self.distance_unit == 'M':
+            return (self.distance / 2) * 201.168
+
+    def update(self):
+        self.dollar_odds = self.dollar_odds / 100
+        self.finish_time = int(self.finish_time) / 1000
+
+
+class Horse:
+    """
+    Nessecary features and information about each horse (ie. Name, date of birth) must be extracted from
+        <Starter/Horse>, however past performance data (ie. finsih time) must be extracted from
+        <Starter/PastPerformances/Start> so this class requires a hierarchal storing structure
+    """
+    past_performances: list[PastPerformance]
+
+    def __init__(self):
+        self.reg_num = 0
+        self.name = ''
+        # self.breed_type = ''
+        self.date_of_birth = ''  # year of birth changed to foaling date
+        self.sex = ''
+        self.past_performances = []
+        # self.name = ''
+        # self.equip = ''
+        # self.weight = 0.0
+        # self.post_position = ''
+        # self.age = 0
+        # self.jockey_key = 0
+        # self.trainer_key = 0
+        # self.breed_type = ''
+        # self.last_pp = ''
+        # self.comments = ''
+        # self.past_performances = []
+
+    @staticmethod
+    def xml_headers_mapping():
+        return {
+            'RegistrationNumber': ('reg_num', str),
+            'HorseName': ('name', str),
+            # 'BreedType/Value': ('breed_type', str),
+            'FoalingDate': ('date_of_birth', str),
+            'Sex/Value': ('sex', str)
+        }
+        # {
+        #     'Starters/Horse/HorseName': ('name', str), 'Starters/Equipment': ('equip', str),
+        #     'Starters/WeightCarried': ('weight', float), 'Starters/PostPosition': ('post_position', str),
+        #     'Starters/Horse/Year': ('age', int), 'Starters/Jockey/ExternalPartyId': ('jokey_key', int),
+        #     'Starters/Trainer/ExternalPartyId': ('trainer_key', int), 'Starters/PastPerformance': ('last_pp', str),
+        #     'Starters/Medication': ('meds', str), 'Starters/Odds': ('odds', float),
+        #     'Starters/Horse/BreedType/Value': ('breed_type', str),
+        #     'Starters/CommentText': ('comments', str)
+        # }
+
+    def to_list(self):
         ...
+
+    def find_age(self, race_date: datetime):
+        age = race_date - datetime.strptime(self.date_of_birth, '%Y-%m-%d+%H:%M')
+        return age.days / 365
+
+    def to_csv_format(self) -> list[dict]:
+        """
+        :return: This function returns a list with all the past races this horse has competed in
+        """
+        all_pps = []
+        csv_dict = {
+            'reg_num': self.reg_num,
+            'sex': self.sex
+        }
+
+        for race in self.past_performances:
+            cur_csv_dict = csv_dict.copy()
+            cur_csv_dict['age'] = self.find_age(datetime.strptime(race.race_date, '%Y-%m-%d+%H:%M'))
+            cur_csv_dict.update(race.to_csv_format())
+            cur_csv_dict['distance'] = race.convert_distance()
+            del cur_csv_dict['wind_dir']
+            del cur_csv_dict['wind_speed']
+            del cur_csv_dict['distance_unit']
+            del cur_csv_dict['race_number']
+            all_pps.append(cur_csv_dict)
+        return all_pps
+
+    def add_pp(self, new_pp: PastPerformance):
+        self.past_performances.append(new_pp)
